3a9919f27d6503fbd35c93dbea5d12ae5bc56fba
Russell Crowe <rasul.amirhan@gmail.com>
2024-01-18 19:10:38 +0600

    version 4

--- src/Homework/Hw_150124_1/Practice.java
+++ src/Homework/Hw_150124_1/Practice.java
@@ -5,12 +5,9 @@ import java.util.Scanner;
 public class Practice {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Пожалуйста, введите сумму перевода в тенге.");
-        int amount = scanner.nextInt(); // считываем сумму перевода
-
+        int amount = scanner.nextInt();
         boolean isValid = TransactionValidator.isValidAmount(amount);
-
         if (isValid == true) {
             System.out.println("Спасибо! Ваш перевод на сумму " + amount + " тг. успешно выполнен.");
         }
