658fe415adab8dac7ac9a0b22c77586ccc017c40
Russell Crowe <rasul.amirhan@gmail.com>
2024-01-29 15:00:00 +0600

    version 5

---
+++ src/Homework/Hw_260124_1/Products.java
@@ -0,0 +1,31 @@
+package Homework.Hw_260124_1;
+
+public class Products {
+    String pizza;
+    String tea;
+    String cookies;
+
+    public String getPizza() {
+        return pizza;
+    }
+
+    public void setPizza(String pizza) {
+        this.pizza = pizza;
+    }
+
+    public String getTea() {
+        return tea;
+    }
+
+    public void setTea(String tea) {
+        this.tea = tea;
+    }
+
+    public String getCookies() {
+        return cookies;
+    }
+
+    public void setCookies(String cookies) {
+        this.cookies = cookies;
+    }
+}
